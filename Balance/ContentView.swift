c8ccdb49cc8dd07ca374194281708dba8d7a8a8e
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2020-07-06 17:42:42 +0100

    Initial Commit

---
+++ Balance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Balance
+//
+//  Created by Hannah Billingsley-Dadd on 06/07/2020.
+//  Copyright © 2020 hannah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
